a12d3173b463623c6438ba949bfe701f38f4be9d
김명진 <mobinity.kmj@gmail.com>
2021-03-25 19:01:45 +0900

    [Add] 회원가입 약관 동의 화면

--- app/src/main/java/com/mobinity/registerusingnavgraph/Fragment1.kt
+++ app/src/main/java/com/mobinity/registerusingnavgraph/Fragment1.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Toast
 import androidx.navigation.Navigation
 
 class Fragment1 : Fragment() {
@@ -21,12 +23,63 @@ class Fragment1 : Fragment() {
 
         val btn = view.findViewById<Button>(R.id.btn)
 
-        btn.setOnClickListener {
-            val action = Fragment1Directions.actionFragment1ToFragment2(123456)
-            Navigation.findNavController(view).navigate(action)
+
+
+        val checkBox1 = view.findViewById<CheckBox>(R.id.cb_1)
+        val checkBox2 = view.findViewById<CheckBox>(R.id.cb_2)
+        val checkBox3 = view.findViewById<CheckBox>(R.id.cb_3)
+        val checkBox4 = view.findViewById<CheckBox>(R.id.cb_4)
+
+
+        checkBox1.setOnCheckedChangeListener { _, isChecked ->
+
+            if(isChecked){
+                checkBox2.isChecked = true
+                checkBox3.isChecked = true
+                checkBox4.isChecked = true
+            }
+
+            else{
+
+                checkBox2.isChecked = false
+                checkBox3.isChecked = false
+                checkBox4.isChecked = false
+            }
+
+            btnCheck(view, btn, checkBox2, checkBox3, checkBox4)
+
         }
 
+        checkBox2.setOnCheckedChangeListener { _, _ -> btnCheck(view, btn, checkBox2, checkBox3, checkBox4) }
+        checkBox3.setOnCheckedChangeListener { _, _ -> btnCheck(view, btn, checkBox2, checkBox3, checkBox4) }
+        checkBox4.setOnCheckedChangeListener { _, _ -> btnCheck(view, btn, checkBox2, checkBox3, checkBox4) }
+
+
         return view
+    }
+
+
+    private fun btnCheck(view: View, btn: Button, checkBox2: CheckBox, checkBox3: CheckBox, checkBox4: CheckBox){
+
+        if(checkBox2.isChecked && checkBox3.isChecked && checkBox4.isChecked){
+
+            btn.setBackgroundColor(requireContext().getColor(R.color.black))
+
+            btn.setOnClickListener {
+                val action = Fragment1Directions.actionFragment1ToFragment2(123456)
+                Navigation.findNavController(view).navigate(action)
+            }
+
+        }
+
+        else {
+
+            btn.setOnClickListener {
+                Toast.makeText(requireContext(), "no", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
 
 }
