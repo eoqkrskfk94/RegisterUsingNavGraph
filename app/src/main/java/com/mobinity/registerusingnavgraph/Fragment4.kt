04387e07a89efdc2c2ebf24984079ce244e685c9
김명진 <mobinity.kmj@gmail.com>
2021-03-29 18:21:19 +0900

    [Add] 신분증 등록 화면

--- app/src/main/java/com/mobinity/registerusingnavgraph/Fragment4.kt
+++ app/src/main/java/com/mobinity/registerusingnavgraph/Fragment4.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.navigation.Navigation
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.mobinity.registerusingnavgraph.databinding.Fragment4Binding
 import java.text.SimpleDateFormat
@@ -26,14 +28,31 @@ class Fragment4 : Fragment() {
 
             val builder = MaterialDatePicker.Builder.datePicker()
             val picker = builder.build()
-            val dateFormat = SimpleDateFormat("yyyy.MM.dd")
+            val dateFormat = SimpleDateFormat("yyyy년 MM월 dd일")
 
             picker.show(activity?.supportFragmentManager!!, picker.toString())
+
+            picker.addOnPositiveButtonClickListener {
+
+                binding.edBirthday.setText(dateFormat.format(it))
+
+            }
         }
 
+        binding.btnNext.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_fragment4_to_fragment5)
+        }
 
+        setGenderEditText()
 
         return view
+    }
+
+    private fun setGenderEditText(){
+        val genders = listOf("남성", "여성")
+        val adapter = ArrayAdapter(requireContext(), R.layout.gender_list, genders)
+        binding.actvGender.setAdapter(adapter)
+
     }
 
 
---
+++ app/src/main/java/com/mobinity/registerusingnavgraph/Fragment5.kt
@@ -0,0 +1,60 @@
+package com.mobinity.registerusingnavgraph
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.FileProvider
+import com.mobinity.registerusingnavgraph.databinding.Fragment5Binding
+import java.io.File
+
+class Fragment5 : Fragment() {
+
+    private var _binding: Fragment5Binding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var imageFile: File
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = Fragment5Binding.inflate(inflater, container, false)
+        val view = binding.root
+
+        cameraBtnClick()
+
+        return view
+    }
+
+    private fun cameraBtnClick(){
+
+//        val imageFileName = "sample" + System.currentTimeMillis().toString()
+//        imageFile = FileLib.getInstance()!!.getImageFile(this, imageFileName)
+//
+//        binding.ivIdentificationCard.setOnClickListener {
+//
+//            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//            //Android 7.0 이상부터 변경
+//            val uri = FileProvider.getUriForFile(requireContext(), requireContext().packageName + ".share", imageFile)
+//            //currentCameraUri = uri
+//
+//            intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+//            startActivityForResult(intent, 0)
+//
+//
+//
+//        }
+
+    }
+
+//    fun getImageFile(context: Context, name: String): File {
+//        return File(getFileDir(context), "$name.png")
+//    }
+
+}
