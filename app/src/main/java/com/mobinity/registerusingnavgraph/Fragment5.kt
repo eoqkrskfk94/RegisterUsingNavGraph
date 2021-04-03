99a4aeb8f365b5fa1703c6a3da3f98cb5f6ce3bd
김명진 <mobinity.kmj@gmail.com>
2021-04-03 15:41:34 +0900

    [Add] 카메라 찍기 가능

--- app/src/main/java/com/mobinity/registerusingnavgraph/Fragment5.kt
+++ app/src/main/java/com/mobinity/registerusingnavgraph/Fragment5.kt
@@ -1,13 +1,17 @@
 package com.mobinity.registerusingnavgraph
 
+import android.app.Activity.RESULT_OK
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.FileProvider
 import com.mobinity.registerusingnavgraph.databinding.Fragment5Binding
 import java.io.File
@@ -16,6 +20,7 @@ class Fragment5 : Fragment() {
 
     private var _binding: Fragment5Binding? = null
     private val binding get() = _binding!!
+    private val REQUEST_IMAGE_CAPTURE = 1
 
     private lateinit var imageFile: File
 
@@ -27,10 +32,27 @@ class Fragment5 : Fragment() {
         _binding = Fragment5Binding.inflate(inflater, container, false)
         val view = binding.root
 
+        cameraBtnClick()
+
         return view
     }
 
     private fun cameraBtnClick(){
+
+        binding.cvIdentificationCard.setOnClickListener {
+
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            } catch (e: ActivityNotFoundException) {
+                // display error state to the user
+                Toast.makeText(context, "error", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
 
 //        val imageFileName = "sample" + System.currentTimeMillis().toString()
 //        imageFile = FileLib.getInstance()!!.getImageFile(this, imageFileName)
@@ -49,6 +71,13 @@ class Fragment5 : Fragment() {
 //
 //        }
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.ivIdentificationCard.setImageBitmap(imageBitmap)
+        }
     }
 
 //    fun getImageFile(context: Context, name: String): File {
--- app/src/main/java/com/mobinity/registerusingnavgraph/MainActivity.kt
+++ app/src/main/java/com/mobinity/registerusingnavgraph/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.mobinity.registerusingnavgraph
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val MY_CAMERA_REQUEST_CODE = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        getPermissions()
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
+
+    }
+
+    private fun getPermissions(){
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), MY_CAMERA_REQUEST_CODE);
+        }
 
     }
 }
